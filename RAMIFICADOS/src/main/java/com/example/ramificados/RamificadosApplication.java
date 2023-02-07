644a3cdf4015fbaff9f34a7dfa13dce5d59778d4
JoseManuelVeraGordillo <95566034+JoseManuelVeraGordillo@users.noreply.github.com>
2023-02-07 10:08:13 +0100

    INICIAL_DEVELOP

---
+++ RAMIFICADOS/src/main/java/com/example/ramificados/RamificadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramificados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamificadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamificadosApplication.class, args);
+    }
+
+}
